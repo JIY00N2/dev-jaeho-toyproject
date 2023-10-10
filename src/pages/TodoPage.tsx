403da6f898b5fecffbbdd4a3f89ba3109b6f3add
JiYoon <angella990825@gmail.com>
2023-10-11 00:19:19 +0900

    feat: 추가만 되는 todo

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,5 +1,80 @@
+import { Button } from '@chakra-ui/button';
+import { Input } from '@chakra-ui/input';
+import { Flex, ListItem, Text, UnorderedList } from '@chakra-ui/layout';
+import { useState, useEffect } from 'react';
+import type { FormEventHandler } from 'react';
+import axios from 'axios';
+
 const TodoPage = () => {
-  return <>Todo 페이지</>;
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todo, setTodo] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('/todos')
+      .then((res) => {
+        setTodos(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('데이터 가져오기 실패', error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .post('/todos', todo, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(() => {
+        axios
+          .get('/todos')
+          .then((response) => {
+            setTodo('');
+            setTodos(response.data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error('데이터 가져오기 실패:', error);
+            setLoading(false);
+          });
+      })
+      .catch((error) => {
+        console.error('데이터 전송 실패:', error);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Flex flexDirection={'column'}>
+      <Text>Todo</Text>
+      <UnorderedList>
+        {todos.map((todo, id) => (
+          <ListItem key={id}>{todo}</ListItem>
+        ))}
+      </UnorderedList>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="todo"
+          placeholder="new todo"
+          disabled={loading}
+          value={todo}
+          onChange={({ target: { value } }) => setTodo(value)}
+        />
+        <Button type={'submit'} disabled={!todo}>
+          <Text>추가</Text>
+        </Button>
+      </form>
+    </Flex>
+  );
 };
 
 export default TodoPage;
