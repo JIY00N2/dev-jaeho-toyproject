9e2a90bdb747a72572d2f7fc59f269d829de7695
JiYoon <angella990825@gmail.com>
2023-10-10 18:06:02 +0900

    feat: Header 구현(Navbar)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,41 @@
+import {
+  Flex,
+  IconButton,
+  Tab,
+  TabList,
+  Tabs,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-  return <div>header</div>;
+  const { toggleColorMode } = useColorMode();
+  const Icon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <Flex justifyContent={'space-around'}>
+      <Tabs>
+        <TabList>
+          <Tab as={Link} to="/home">
+            Google Map
+          </Tab>
+          <Tab as={Link} to="/infinitescroll">
+            무한 스크롤
+          </Tab>
+          <Tab as={Link} to="/todo">
+            Three
+          </Tab>
+          <IconButton
+            aria-label="toggleDarkMode dark mode"
+            onClick={toggleColorMode}
+            icon={<Icon />}
+            variant={'ghost'}
+          />
+        </TabList>
+      </Tabs>
+    </Flex>
+  );
 };
 export default Header;
