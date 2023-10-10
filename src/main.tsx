dfffbcc6b9d01b16a8e171833c493508d15cf1d5
JiYoon <angella990825@gmail.com>
2023-10-11 00:19:32 +0900

    feat: react query, msw 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from './routes/Router';
 import theme from './styles/theme';
+import { worker } from './mocks/browser';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <RouterProvider router={Router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={Router} />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 );
